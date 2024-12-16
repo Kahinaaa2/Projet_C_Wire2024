477677eff258d853f5adf6ec5db9e2bba075f75f
ashanamahes <167230342+ashanamahes@users.noreply.github.com>
2024-12-16 16:06:52 +0100

    Update C_wire.c

--- C_wire.c
+++ C_wire.c
@@ -1,60 +1,64 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct AVL {
-	long id; 
-	long conso;
-	long cap;
-	int equ;
-	struct AVL* d;
-	struct AVL* g;
+//structure de l'AVL
+typedef struct AVL { 
+	long identifiant; 
+	long consommation;
+	long capacite;
+	int eq;	//facteur d'équilibre
+	struct AVL* fg;
+	struct AVL* fd;
 }AVL; 
 
-long min(long a, long b){
-  
-   if(a > b){
+//fonction pour calculer le mininum
+long min(long a, long b){	
+   if(a>b){
      return b;
    }
    return a;
 }
 
+//fonction pour calculer le maximum
 long max(long a, long b){     
-
-   if(a > b){
+   if(a>b){
      return a;
    }
    return b;
 }
    
-
-
-AVL* creationAVL(long nb, long cap, long conso){
+//création d'un AVL   
+AVL* creerAVL(long nb, long cap, long conso){
 	AVL* new = malloc(sizeof(AVL));
 	if(new == NULL){
 		exit (1);
 	}
 	
-	new->id =nb;
-	new->cap = cap;
-	new->conso = conso;
-	new->equ = 0;
-	new->g = NULL;
-	new->d = NULL;
+	new->identifiant = nb;	//identifiant de la station
+	new->capacite = cap;	//capacité de la station
+	new->consommation = conso;	//consommation 
+	new->eq = 0;
+	new->fg = NULL;
+	new->fd = NULL;
 
 	return new;
 }
 
 AVL* rotationDroite(AVL* a){
+	if(a==NULL){
+	  printf("err1\n");
+	  exit(2);
+	}  
+	
+	AVL* p = a->fg;
+	int eq_a = a->eq;
+	int eq_p = p->eq;
 
-	AVL* p = a->g;
-	int eqa = a->equ;
-	int eqp = p->equ;
+	a->fg = p->fd;
+	p->fd = a;
 
-	a->g = p->d;
-	p->d = a;
-
-	a->equ = eqa - min(eqp, 0) + 1; 
-	p->equ = max(max(eqa + 2, eqa + eqp + 2), eqp + 1);
+	a->eq = eq_a - min(eq_p, 0) + 1; 
+	p->eq = max(max(eq_a + 2, eq_a + eq_p + 2), eq_p + 1);
 
 	return p; 
 }
@@ -62,18 +66,18 @@ AVL* rotationDroite(AVL* a){
 AVL* rotationGauche(AVL* a){
 	if(a==NULL){
 	  printf("err2\n");
-	  exit(2);
+	  exit(3);
 	}  
 	
-	AVL* p = a->d;
-	int eqa = a->equ;
-	int eqp = p->equ;
+	AVL* p = a->fd;
+	int eq_a = a->eq;
+	int eq_p = p->eq;
 
-	a->d = p->g;
-	p->g = a;
+	a->fd = p->fg;
+	p->fg = a;
 
-	a->equ = eqa - max(eqp, 0) - 1;
-	p->equ = min(min(eqa - 2, eqa + eqp - 2), eqp - 1);
+	a->eq = eq_a - max(eq_p, 0) - 1;
+	p->eq = min(min(eq_a - 2, eq_a + eq_p - 2), eq_p - 1);
 
 	return p;
 }
@@ -81,117 +85,115 @@ AVL* rotationGauche(AVL* a){
 AVL* doubleRotationDroite(AVL* a){
 	if(a==NULL){
 	  printf("err3\n");
-	  exit(3);
+	  exit(4);
 	} 
-	a->g = rotationDroite(a->g);
+	a->fg = rotationDroite(a->fg);
 	return rotationGauche(a);
 }
 
 AVL* doubleRotationGauche(AVL* a){
 	if(a==NULL){
 	  printf("err4\n");
-	  exit(4);
+	  exit(5);
 	} 
 	
-	a->d = rotationGauche(a->d);
+	a->fd = rotationGauche(a->fd);
 	return rotationDroite(a);
 }
 
 AVL* equilibreAVL(AVL* a){
-
-
 	if(a==NULL){
 	  printf("err5\n");
-	  exit(5);
+	  exit(6);
 	} 
 
-	if(a->equ <= -2){
-		if(a->g->equ <= 0){
+	if(a->eq <= -2){
+		if(a->fg->eq <= 0){
 			return rotationDroite(a);
 		}
 		else{
 			return doubleRotationDroite(a);
 		}
-	}else if(a->equ >= 2){
-		if(a->d->equ >= 0){
+	}
+	else if(a->eq >= 2){
+		if(a->fd->eq >= 0){
 			return rotationGauche(a);
 		}
 		else{
 			return doubleRotationGauche(a);
 		}
 	}
-	
-
 	return a;
 }	
 
 AVL* insererAVL(AVL* a, long e, long v, long c, int *h){
 	if(a == NULL){
 		*h = 0;
-		a=creationAVL(e, v, c);
-		
-		if(a==NULL){
-	 	   printf("err6\n");
-	 	   exit(6);
-		} 
-		
+		a=creerAVL(e, v, c);	
 		return a;
 	}
-	
-	else if(e<a->id){
-		a->g = insererAVL(a->g, e, v, c, h);
+	else if(e<a->identifiant){
+		a->fg = insererAVL(a->fg, e, v, c, h);
 		*h = -(*h);
 	}
-
-	else if(e>a->id){
-		a->d = insererAVL(a->d, e, v, c, h);
+	else if(e>a->identifiant){
+		a->fd = insererAVL(a->fd, e, v, c, h);
 	}
-	
-	else if(e==a->id){
-		a->conso += c;
-		a->cap +=v;
+	else if(e==a->identifiant){
+		a->consommation += c;
+		a->capacite +=v;
 	}
-	
 	else {
 		*h = 0; 
 		return a; 
 	}
 
 	if(*h != 0){
-	
-		a->equ += *h;
+		a->eq += *h;
 		a = equilibreAVL(a);
 
-		if(a->equ == 0){
+		if(a->eq == 0){
 			*h = 0;
-		}else{
+		}
+		else{
 			*h = 1;
 		}
 	}	
-
 	return a;
 }
 
 void affiche(AVL* a){
    if(a!=NULL){
-     affiche(a->g);
-     printf("%ld;%ld;%ld\n",a->id,a->cap,a->conso);
-     affiche(a->d);
+     affiche(a->fg);
+     printf("%ld;%ld;%ld\n",a->identifiant,a->capacite,a->consommation);
+     affiche(a->fd);
    }
 }     
 
+void libererAVL(AVL* a){
+	if(a!=NULL){
+		libererAVL(a->fg);
+		libererAVL(a->fd);
+		free(a);
+	}
+}
+
 int main(){
-  long a1,a2,a3;
-  AVL* stat = NULL;
+  long v1,v2,v3;
+  AVL* station = NULL;
   int h = 0;
 
-      while(scanf("%ld;%ld;%ld\n",&a1,&a2,&a3) == 3){
-     	 station=insererAVL(station,a1,a2,a3,&h);
-     	 //printf("%ld,%ld,%ld\n",a1,a2,a3);
-      }	 
-      
-  affiche(stat);
+  while(scanf("%ld;%ld;%ld\n",&v1,&v2,&v3) == 3){
+ 	 station=insererAVL(station,v1,v2,v3,&h);
+ 	 //printf("%ld,%ld,%ld\n",v1,v2,v3);
+  }	 
+  
+  affiche(station);
+  libererAVL(station);
   
   return 0;
 }  
 
+
+
+	
