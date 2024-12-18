e949ed538d9feaf91e978679146fb8594695d35b
ashanamahes <167230342+ashanamahes@users.noreply.github.com>
2024-12-18 16:29:52 +0100

    Update and rename C_wire.c to c-wire.c

--- c-wire.c
+++ c-wire.c
@@ -11,7 +11,7 @@ typedef struct AVL {
 	struct AVL* fd;
 }AVL; 
 
-//fonction pour calculer le mininum
+//fonction pour calculer le mininum entre deux valeurs
 long min(long a, long b){	
    if(a>b){
      return b;
@@ -19,7 +19,7 @@ long min(long a, long b){
    return a;
 }
 
-//fonction pour calculer le maximum
+//fonction pour calculer le maximum entre deux valeurs 
 long max(long a, long b){     
    if(a>b){
      return a;
@@ -27,10 +27,11 @@ long max(long a, long b){
    return b;
 }
    
-//création d'un AVL   
+//création d'un nouveau noeud d'AVL   
 AVL* creerAVL(long nb, long cap, long conso){
 	AVL* new = malloc(sizeof(AVL));
 	if(new == NULL){
+		printf("Allocation échouée");
 		exit (1);
 	}
 	
@@ -43,71 +44,71 @@ AVL* creerAVL(long nb, long cap, long conso){
 
 	return new;
 }
-
+//simple rotation droite 
 AVL* rotationDroite(AVL* a){
 	if(a==NULL){
 	  printf("err1\n");
 	  exit(2);
 	}  
 	
-	AVL* pivot = a->fg;
+	AVL* pivot = a->fg; //le fils gauche est le pivot 
 	int eq_a = a->eq;
 	int eq_p = pivot->eq;
 
 	a->fg = pivot->fd;
 	pivot->fd = a;
-
+	//mise à jour des facteurs d'équilibre 
 	a->eq = eq_a - min(eq_p, 0) + 1; 
 	pivot->eq = max(max(eq_a + 2, eq_a + eq_p + 2), eq_p + 1);
 
 	return pivot; 
 }
-
+//simple rotation gauche
 AVL* rotationGauche(AVL* a){
 	if(a==NULL){
 	  printf("err2\n");
 	  exit(3);
 	}  
 	
-	AVL* pivot = a->fd;
+	AVL* pivot = a->fd; //le fils droit est le pivot
 	int eq_a = a->eq;
 	int eq_p = pivot->eq;
 
 	a->fd = pivot->fg;
 	pivot->fg = a;
-
+	//mise à jour des facteurs d'équilibre 
 	a->eq = eq_a - max(eq_p, 0) - 1;
 	pivot->eq = min(min(eq_a - 2, eq_a + eq_p - 2), eq_p - 1);
 
 	return pivot;
 }
-
+//double rotation droite 
 AVL* doubleRotationDroite(AVL* a){
 	if(a==NULL){
 	  printf("err3\n");
 	  exit(4);
 	} 
-	a->fg = rotationDroite(a->fg);
-	return rotationGauche(a);
+	a->fg = rotationDroite(a->fg); 
+	return rotationGauche(a); 
 }
-
+//double rotation gauche
 AVL* doubleRotationGauche(AVL* a){
 	if(a==NULL){
 	  printf("err4\n");
 	  exit(5);
 	} 
 	
-	a->fd = rotationGauche(a->fd);
+	a->fd = rotationGauche(a->fd); //rotation 
 	return rotationDroite(a);
 }
-
+//fonction pour équilibrer l'AVL
 AVL* equilibreAVL(AVL* a){
 	if(a==NULL){
 	  printf("err5\n");
 	  exit(6);
 	} 
 
-	if(a->eq <= -2){
+	if(a->eq <= -2){ // sous-arbre gauche déséquilibré
 		if(a->fg->eq <= 0){
 			return rotationDroite(a);
 		}
@@ -115,7 +116,7 @@ AVL* equilibreAVL(AVL* a){
 			return doubleRotationDroite(a);
 		}
 	}
-	else if(a->eq >= 2){
+	else if(a->eq >= 2){ // sous-arbre droit déséquilibré
 		if(a->fd->eq >= 0){
 			return rotationGauche(a);
 		}
@@ -125,9 +126,9 @@ AVL* equilibreAVL(AVL* a){
 	}
 	return a;
 }	
-
+//fonction d'insertion dans un AVL
 AVL* insererAVL(AVL* a, long e, long v, long c, int *h){
-	if(a == NULL){
+	if(a == NULL){ //si l'arbre est vide, on le creé 
 		*h = 0;
 		a=creerAVL(e, v, c);	
 		return a;
@@ -139,7 +140,7 @@ AVL* insererAVL(AVL* a, long e, long v, long c, int *h){
 	else if(e>a->identifiant){
 		a->fd = insererAVL(a->fd, e, v, c, h);
 	}
-	else if(e==a->identifiant){
+	else if(e==a->identifiant){ //mise à jour des valeurs si l'identifiant existe déjà
 		a->consommation += c;
 		a->capacite +=v;
 	}
@@ -147,7 +148,7 @@ AVL* insererAVL(AVL* a, long e, long v, long c, int *h){
 		*h = 0; 
 		return a; 
 	}
-
+	//rééquilibrage
 	if(*h != 0){
 		a->eq += *h;
 		a = equilibreAVL(a);
@@ -161,7 +162,7 @@ AVL* insererAVL(AVL* a, long e, long v, long c, int *h){
 	}	
 	return a;
 }
-
+//fonction pour afficher les données de chaque noeud de l'AVL
 void affiche(AVL* a){
    if(a!=NULL){
      affiche(a->fg);
@@ -169,7 +170,7 @@ void affiche(AVL* a){
      affiche(a->fd);
    }
 }     
-
+//fonction pour libérer la mémoire allouée
 void libererAVL(AVL* a){
 	if(a!=NULL){
 		libererAVL(a->fg);
@@ -184,8 +185,12 @@ int main(){
   int h = 0;
 
   while(scanf("%ld;%ld;%ld\n",&v1,&v2,&v3) == 3){
+	  //validation des entrées
+	  if(v1<0||v2<0||v3<0){
+		  printf("Données incorrectes");
+		  exit(7);
+	  }
  	 station=insererAVL(station,v1,v2,v3,&h);
- 	 //printf("%ld,%ld,%ld\n",v1,v2,v3);
   }	 
   
   affiche(station);
