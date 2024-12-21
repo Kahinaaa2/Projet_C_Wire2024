ef718f02196fb610f2359b9983361f1d96f98040
melanieevl <melanie.avel78@gmail.com>
2024-12-21 23:12:59 +0100

    Create main.h

---
+++ codeC/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+typedef struct AVL{
+    long identifiant;
+    long consommation;
+    long capacite;
+    int eq;
+    struct AVL* fg;
+    struct AVL* fd;
+}AVL;
+
+#include "insertionavl.h"
+#include "rotationavl.h"
+#include "fonction.h"
+#endif
