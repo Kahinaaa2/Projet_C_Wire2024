807a5d985227cce289ea57c4d9315cc2df537e77
melanieevl <melanie.avel78@gmail.com>
2024-12-21 23:14:31 +0100

    Create rotationavl.h

---
+++ codeC/rotationavl.h
@@ -0,0 +1,14 @@
+#ifndef ROTATIONAVL_H
+#define ROTATIONAVL_H
+
+#include "main.h"
+#include "insertionavl.h"
+#include "fonction.h"
+
+AVL* rotationDroite(AVL* a);
+AVL* rotationGauche(AVL* a);
+AVL* doubleRotationDroite(AVL* a);
+AVL* doubleRotationGauche(AVL* a);
+AVL* equilibreAVL(AVL* a);
+
+#endif
