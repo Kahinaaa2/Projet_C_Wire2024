c741c3e729789cbfd35f15d958f4f26e42030fbe
melanieevl <melanie.avel78@gmail.com>
2024-12-22 23:54:23 +0100

    Update main.c

--- codeC/main.c
+++ codeC/main.c
@@ -2,15 +2,15 @@
 
 
 // Lecture des données en bloc pour optimisation
-void lire_donnees_en_bloc(AVL** station) {
-    char buffer[BUFFER_SIZE];
+void lireDonnees(AVL** station) {
+    char recup[1024];
     int h = 0;
     long v1, v2, v3;
 
-    while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
-        if (sscanf(buffer, "%ld;%ld;%ld", &v1, &v2, &v3) == 3) {
+    while (fgets(recup, 1024, stdin) != NULL) {
+        if (sscanf(recup, "%ld;%ld;%ld", &v1, &v2, &v3) == 3) {
             if (v1 < 0 || v2 < 0 || v3 < 0) {
-                fprintf(stderr, "Erreur : Données incorrectes\n");
+                printf("Les données du fichiers sont incorrectes et ne correspondent pas\n");
                 exit(7);
             }
             *station = insererAVL(*station, v1, v2, v3, &h);
@@ -22,7 +22,7 @@ int main() {
     AVL* station = NULL;
 
     // Lecture et insertion des données
-    lire_donnees_en_bloc(&station);
+    lireDonnees(&station);
 
     // Affichage des résultats
     affiche(station);
