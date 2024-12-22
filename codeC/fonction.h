77d8e410b23690aba52f9a247c603b0259c60afb
melanieevl <melanie.avel78@gmail.com>
2024-12-22 21:58:17 +0100

    Update fonction.h

--- codeC/fonction.h
+++ codeC/fonction.h
@@ -2,13 +2,10 @@
 #define FONCTION_H
 
 #include "main.h"
-#include "insertionavl.h"
-#include "rotationavl.h"
 
 long min(long a, long b);
 long max(long a, long b);
 void affiche(AVL* a);
 void libererAVL(AVL* a);
-void lire_donnees_en_bloc(AVL** station);
 
 #endif
