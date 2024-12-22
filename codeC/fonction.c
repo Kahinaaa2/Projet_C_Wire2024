3d11f41c54e5618d1ca8fb9ee660944103abd6dc
melanieevl <melanie.avel78@gmail.com>
2024-12-22 21:57:53 +0100

    Update fonction.c

--- codeC/fonction.c
+++ codeC/fonction.c
@@ -27,20 +27,3 @@ void libererAVL(AVL* a) {
         free(a);
     }
 }
-
-// Lecture des données en bloc pour optimisation
-void lire_donnees_en_bloc(AVL** station) {
-    char buffer[BUFFER_SIZE];
-    int h = 0;
-    long v1, v2, v3;
-
-    while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
-        if (sscanf(buffer, "%ld;%ld;%ld", &v1, &v2, &v3) == 3) {
-            if (v1 < 0 || v2 < 0 || v3 < 0) {
-                fprintf(stderr, "Erreur : Données incorrectes\n");
-                exit(7);
-            }
-            *station = insererAVL(*station, v1, v2, v3, &h);
-        }
-    }
-}
